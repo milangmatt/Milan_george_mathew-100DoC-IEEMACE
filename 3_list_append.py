92dd7eba9283700a2c4c21d2be39bcfcfec094c5
Milangm <milangeorgem@gmail.com>
2023-03-26 16:19:45 +0530

    3rd done

--- 3_list_append.py
+++ 3_list_append.py
@@ -1,7 +1,12 @@
-def list_append(input_list):
-    add_list=eval(input("enter list of numbers: "))
-    if type(add_list)==int
-    input_list.extend(add_list)
-    return(input_list)
-
-print(list_append([1,2,3]))
+list=[1,2,3]
+nums=eval(input("Enter the numbers separated by commas : "))
+if type(nums)==int:
+    list.append(nums)
+else:
+    for i in nums:
+        if str(i).isdigit():
+            number=int(i)
+            list.append(number)
+        else:
+            continue
+print(list)
